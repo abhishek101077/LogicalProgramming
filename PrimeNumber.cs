8c5b71d9058dc322eb7f8b103bfdd0fa7b51c973
Abhishek shukla <Abhishekgargshu786@gmail.com>
2023-03-18 21:48:00 +0530

    The programme for prime number

---
+++ PrimeNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    internal class PrimeNumber
+    {
+        public static void solution()
+        {
+            Console.WriteLine("Enter a number to check and confirm if its a prime number or not ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            for (int i = 2; i < num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 0)
+            {
+                Console.WriteLine($"{num} is a Prime number");
+            }
+            else
+            {
+                Console.WriteLine($"{num} is a Non-Prime number");
+
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
