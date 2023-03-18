b97e6aa8ba7b11582e0e4d309f2c9d0d2947d016
Abhishek shukla <Abhishekgargshu786@gmail.com>
2023-03-18 21:14:19 +0530

    The programme for Fibonacci series

---
+++ FIbonacci.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    internal class FIbonacci
+    {
+        public static void solution()
+        {
+            int i, count, f1 = 0, f2 = 1, f3 = 0;
+            Console.Write("Enter the Value: ");
+            count = int.Parse(Console.ReadLine());
+            Console.WriteLine(f1);
+            Console.WriteLine(f2);
+            for (i = 0; i <= count; i++)
+            {
+                f3 = f1 + f2;
+                Console.WriteLine(f3);
+                f1 = f2;
+                f2 = f3;
+            }
+            Console.ReadLine();
+        }
+
+
+        }
+    }
